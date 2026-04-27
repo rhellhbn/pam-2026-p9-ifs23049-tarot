724aef95f4605359abd70fd4a386a55954ac927f
rhellhbn <164168446+rhellhbn@users.noreply.github.com>
2026-04-27 20:24:08 +0700

    Update api_constants.dart

--- lib/core/constants/api_constants.dart
+++ lib/core/constants/api_constants.dart
@@ -1,5 +1,5 @@
 class ApiConstants {
-  static const String baseUrl = "http://127.0.0.1:8080";
+  static const String baseUrl = "https://pam-2026-p9-ifs23049-be.hellhbn.fun:8080/";
 
   static const String login = "$baseUrl/auth/login";
   static const String me = "$baseUrl/auth/me";
